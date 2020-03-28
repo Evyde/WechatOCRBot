813a7e5f3df5e874323c56a71d339adf9682a2ca
shinydog666 <58540850+shinydog666@users.noreply.github.com>
2020-03-28 12:33:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-	from PIL import ImageGrab
-	from PIL import Image
+from PIL import ImageGrab
+from PIL import Image
+	
+leftborder = L
+rightborder = R
+upborder = U
+downborder = D
+	
+pic = ImageGrab.grab(bbox=(L,R,U,D))
+pic.save(saveFileName)
